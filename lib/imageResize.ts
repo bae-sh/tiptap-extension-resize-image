34e89f3266585b723a8e8c5a769c75c31e9a7157
DongYounYim <dyyim4725@gmail.com>
2023-12-12 15:02:10 +0900

    [chore] 오타 및 불필요한 변수 제거

--- lib/imageResize.ts
+++ lib/imageResize.ts
@@ -1,4 +1,4 @@
-import Image from '@tiptap/extension-image';
+import Image from "@tiptap/extension-image";
 
 export const ImageResize = Image.extend({
   addAttributes() {
@@ -10,7 +10,7 @@ export const ImageResize = Image.extend({
         default: null,
       },
       style: {
-        default: 'width: 100%; height: 100%;',
+        default: "width: 100%; height: auto;",
       },
     };
   },
@@ -21,41 +21,44 @@ export const ImageResize = Image.extend({
         options: { editable },
       } = editor;
       const { src, alt, style } = node.attrs;
-      const $contatiner = document.createElement('div');
-      const $img = document.createElement('img');
+      const $container = document.createElement("div");
+      const $img = document.createElement("img");
 
-      $contatiner.appendChild($img);
-      $img.setAttribute('src', src);
-      $img.setAttribute('alt', alt);
-      $img.setAttribute('style', style);
-      $img.setAttribute('draggable', 'true');
+      $container.appendChild($img);
+      $img.setAttribute("src", src);
+      $img.setAttribute("alt", alt);
+      $img.setAttribute("style", style);
+      $img.setAttribute("draggable", "true");
 
       if (!editable) return { dom: $img };
 
       const dotsPosition = [
-        'top: -4px; left: -4px; cursor: nwse-resize;',
-        'top: -4px; right: -4px; cursor: nesw-resize;',
-        'bottom: -4px; left: -4px; cursor: nesw-resize;',
-        'bottom: -4px; right: -4px; cursor: nwse-resize;',
+        "top: -4px; left: -4px; cursor: nwse-resize;",
+        "top: -4px; right: -4px; cursor: nesw-resize;",
+        "bottom: -4px; left: -4px; cursor: nesw-resize;",
+        "bottom: -4px; right: -4px; cursor: nwse-resize;",
       ];
-      $contatiner.setAttribute('style', `position: relative; border: 1px dashed #6C6C6C; ${style}`);
+      $container.setAttribute(
+        "style",
+        `position: relative; border: 1px dashed #6C6C6C; ${style}`
+      );
 
       let isResizing = false;
       let startX: number, startWidth: number, startHeight: number;
 
       Array.from({ length: 4 }, (_, index) => {
-        const $dot = document.createElement('div');
+        const $dot = document.createElement("div");
         $dot.setAttribute(
-          'style',
-          `position: absolute; width: 9px; height: 9px; border: 1.5px solid #6C6C6C; border-radius: 50%; ${dotsPosition[index]}`,
+          "style",
+          `position: absolute; width: 9px; height: 9px; border: 1.5px solid #6C6C6C; border-radius: 50%; ${dotsPosition[index]}`
         );
 
-        $dot.addEventListener('mousedown', e => {
+        $dot.addEventListener("mousedown", (e) => {
           e.preventDefault();
           isResizing = true;
           startX = e.clientX;
-          startWidth = $contatiner.offsetWidth;
-          startHeight = $contatiner.offsetHeight;
+          startWidth = $container.offsetWidth;
+          startHeight = $container.offsetHeight;
 
           const onMouseMove = (e: MouseEvent) => {
             if (!isResizing) return;
@@ -66,39 +69,39 @@ export const ImageResize = Image.extend({
             const newWidth = startWidth + deltaX;
             const newHeight = newWidth / aspectRatio;
 
-            $contatiner.style.width = newWidth + 'px';
-            $contatiner.style.height = newHeight + 'px';
+            $container.style.width = newWidth + "px";
+            $container.style.height = newHeight + "px";
 
-            $img.style.width = newWidth + 'px';
-            $img.style.height = newHeight + 'px';
+            $img.style.width = newWidth + "px";
+            $img.style.height = newHeight + "px";
           };
 
           const onMouseUp = () => {
             if (isResizing) {
               isResizing = false;
             }
-            if (typeof getPos === 'function') {
+            if (typeof getPos === "function") {
               const newAttrs = {
                 ...node.attrs,
-                width: $img.width,
-                height: $img.height,
                 style: `${$img.style.cssText}`,
               };
-              view.dispatch(view.state.tr.setNodeMarkup(getPos(), null, newAttrs));
+              view.dispatch(
+                view.state.tr.setNodeMarkup(getPos(), null, newAttrs)
+              );
             }
 
-            document.removeEventListener('mousemove', onMouseMove);
-            document.removeEventListener('mouseup', onMouseUp);
+            document.removeEventListener("mousemove", onMouseMove);
+            document.removeEventListener("mouseup", onMouseUp);
           };
 
-          document.addEventListener('mousemove', onMouseMove);
-          document.addEventListener('mouseup', onMouseUp);
+          document.addEventListener("mousemove", onMouseMove);
+          document.addEventListener("mouseup", onMouseUp);
         });
-        $contatiner.appendChild($dot);
+        $container.appendChild($dot);
       });
 
       return {
-        dom: $contatiner,
+        dom: $container,
       };
     };
   },
