fe2cac3ca5068450adb27c7828c08d69fd6b6692
Im-younique <dyyim4725@gmail.com>
2024-06-03 21:24:00 +0900

    feat: Add alignment controller mouse event

--- lib/imageResize.ts
+++ lib/imageResize.ts
@@ -108,25 +108,42 @@ export const ImageResize = Image.extend({
         const $centerController = document.createElement('img');
         const $rightController = document.createElement('img');
 
+        const controllerMouseOver = e => {
+          e.target.style.opacity = 0.3;
+        };
+
+        const controllerMouseOut = e => {
+          e.target.style.opacity = 1;
+        };
+
         $postionController.setAttribute(
           'style',
           'position: absolute; top: 0%; left: 50%; width: 100px; height: 25px; z-index: 999; background-color: rgba(255, 255, 255, 0.7); border-radius: 4px; border: 2px solid #6C6C6C; cursor: pointer; transform: translate(-50%, -50%); display: flex; justify-content: space-between; align-items: center; padding: 0 10px;',
         );
+
         $leftController.setAttribute(
           'src',
           'https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsoutlined/format_align_left/default/20px.svg',
         );
         $leftController.setAttribute('style', iconStyle);
+        $leftController.addEventListener('mouseover', controllerMouseOver);
+        $leftController.addEventListener('mouseout', controllerMouseOut);
+
         $centerController.setAttribute(
           'src',
           'https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsoutlined/format_align_center/default/20px.svg',
         );
         $centerController.setAttribute('style', iconStyle);
+        $centerController.addEventListener('mouseover', controllerMouseOver);
+        $centerController.addEventListener('mouseout', controllerMouseOut);
+
         $rightController.setAttribute(
           'src',
           'https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsoutlined/format_align_right/default/20px.svg',
         );
         $rightController.setAttribute('style', iconStyle);
+        $rightController.addEventListener('mouseover', controllerMouseOver);
+        $rightController.addEventListener('mouseout', controllerMouseOut);
 
         $leftController.addEventListener('click', () => {
           $img.setAttribute('style', `${$img.style.cssText} margin: 0 auto 0 0;`);
