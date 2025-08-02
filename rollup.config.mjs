2401ef672d1501dff162a009cb87ea5f4cd48524
bae-sh <hyeon1293@gmail.com>
2025-08-03 04:29:36 +0900

    update: modify rollup files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,46 +1,17 @@
-import typescript from '@rollup/plugin-typescript';
-import tslib from 'tslib';
+import typescript from 'rollup-plugin-typescript2';
 
 export default [
   {
     input: 'lib/index.ts',
     output: [
-      {
-        dir: './dist',
-        format: 'cjs',
-        sourcemap: true,
-        exports: 'auto',
-      },
+      { dir: 'dist', format: 'cjs', sourcemap: true, exports: 'auto' },
+      { dir: 'esm', format: 'esm', sourcemap: true, exports: 'auto' },
     ],
     external: ['@tiptap/core', '@tiptap/extension-image', '@tiptap/pm', 'tslib'],
     plugins: [
       typescript({
-        tslib,
         tsconfig: './tsconfig.json',
-      }),
-    ],
-  },
-  {
-    input: 'lib/index.ts',
-    output: [
-      {
-        dir: './esm',
-        format: 'esm',
-        sourcemap: true,
-        exports: 'auto',
-      },
-    ],
-    external: ['@tiptap/core', '@tiptap/extension-image', '@tiptap/pm', 'tslib'],
-    plugins: [
-      typescript({
-        tslib,
-        tsconfig: './tsconfig.json',
-        compilerOptions: {
-          outDir: './esm',
-          composite: false,
-          declaration: false,
-          declarationMap: false,
-        },
+        useTsconfigDeclarationDir: true,
       }),
     ],
   },
